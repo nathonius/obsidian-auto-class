635bc49b6adb216390f1fce972904d86e3c49849
OfficerHalf <nathan@nathan-smith.org>
2021-11-17 00:42:58 -0500

    doc: add an example tag match for first run

--- src/constants.ts
+++ src/constants.ts
@@ -2,6 +2,9 @@ import { ClassMatchScope } from './enum';
 import { AutoClassPluginSettings } from './interfaces';
 
 export const DEFAULT_SETTINGS: AutoClassPluginSettings = {
-  matches: [{ path: 'Example Path/Subfolder/', classes: ['example-class'], scope: ClassMatchScope.Preview }],
+  matches: [
+    { path: 'Example Path/Subfolder/', classes: ['example-class'], scope: ClassMatchScope.Preview },
+    { tag: '#ExampleTag', classes: ['another-class'], scope: ClassMatchScope.Edit }
+  ],
   version: '2.0.0'
 };
