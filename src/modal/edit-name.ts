165b010f0c1e9f2da6d2118609d2283b77196d62
Nathan Smith <nathan@nathan-smith.org>
2023-02-26 20:00:36 -0500

    refactor: move edit name modal to preact

--- src/modal/edit-name.ts
+++ src/modal/edit-name.ts
@@ -1,22 +1,51 @@
 import { Modal } from 'obsidian';
+import { FunctionComponent, render } from 'preact';
+import { useEffect, useRef } from 'preact/hooks';
+import { className, html } from '../util';
+
+const c = className('auto-class-edit-name');
 
 export class EditNameModal extends Modal {
   currentName = '';
   callback: (newName: string) => void;
 
   onOpen(): void {
+    this.titleEl.innerHTML = 'Edit Group Name';
     this.contentEl.empty();
-    const input = this.contentEl.createEl('input', { cls: 'auto-class-edit-name__input', attr: { type: 'text' } });
-    input.value = this.currentName;
-    const controls = this.contentEl.createDiv({ cls: 'auto-class-edit-name__controls' });
-    const confirmButton = controls.createEl('button', { text: 'Save', cls: 'mod-cta' });
-    confirmButton.addEventListener('click', () => {
-      this.callback(input.value);
-      this.close();
-    });
-    const cancelButton = controls.createEl('button', { text: 'Cancel' });
-    cancelButton.addEventListener('click', () => {
-      this.close();
-    });
+    render(
+      html`<${ModalContent}
+        confirm=${(value: string) => {
+          this.callback(value);
+          this.close();
+        }}
+        cancel=${this.close.bind(this)}
+        value=${this.currentName}
+      />`,
+      this.contentEl
+    );
   }
 }
+
+const ModalContent: FunctionComponent<{ confirm: (value: string) => void; cancel: () => void; value: string }> = (
+  props
+) => {
+  const inputRef = useRef<HTMLInputElement>(null);
+  useEffect(() => {
+    if (inputRef.current) {
+      inputRef.current.value = props.value;
+    }
+  }, [inputRef.current, props.value]);
+  return html`<input type="text" class=${c('input')} ref=${inputRef} />
+    <div class=${c('controls')}>
+      <button
+        type="button"
+        class="mod-cta"
+        onClick=${() => {
+          props.confirm(inputRef.current.value);
+        }}
+      >
+        Save
+      </button>
+      <button type="button" onClick=${props.cancel}>Cancel</button>
+    </div> `;
+};
